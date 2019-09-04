2576b7a5ccdca26f6c62dc7e83bad4e1d4ce6785
hide <snowbear.github@gmail.com>
2019-09-04 18:21:39 +0000

    add boostrap design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+    @tasks = Task.all.page(params[:page]).per(3)
   end
 
   def show
