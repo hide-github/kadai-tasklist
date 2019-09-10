8a1b3e5567e86a40f5d6d0039f0a60ac71d362ec
hide <snowbear.github@gmail.com>
2019-09-10 08:47:59 +0000

    add correct_user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index ,:show, :new, :edit]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -55,4 +56,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+     redirect_to root_url
+    end
+  end
 end
